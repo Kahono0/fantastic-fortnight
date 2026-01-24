79a105bb202d3eb49cc249affe62c68706ffc214
kahono <70957978+Kahono0@users.noreply.github.com>
2026-01-24 20:19:35 +0300

    Enhance install script for Node.js and NVM setup
    This script now checks for NVM installation, installs it if missing, and installs Node.js LTS version. It also verifies the installations and clones a repository for further setup.

--- install.sh
+++ install.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-echo "Starting installation process..."
+# This script installs Node.js and npm on a Mac os system
+echo "Checking NVM installation..."
+if ! command -v nvm &> /dev/null
+then
+    echo "NVM not found. Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+else
+    echo "NVM is already installed."
+fi
+
+echo "Installing Node.js LTS version..."
+nvm install --lts
+
+echo "Setting Node.js LTS version as default..."
+nvm use --lts
+nvm alias default 'lts/*'
+echo "Node.js and npm installation complete."
+
+echo "Verifying installations..."
+node_version=$(node -v)
+npm_version=$(npm -v)
+echo "Node.js version: $node_version"
+echo "npm version: $npm_version"
+echo "Installation successful!"
+
+echo "Starting app installation process..."
 echo "Cloning repository..."
 git clone git@github.com:Kahono0/fantastic-fortnight.git issue-app
 cd issue-app || { echo "Failed to enter directory"; exit 1; }
